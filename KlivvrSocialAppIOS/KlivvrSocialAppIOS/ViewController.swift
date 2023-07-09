fd93e1863842ad306df2f02a504f287a2e902e62
Eman <emanm7260@gmail.com>
2023-07-09 17:48:36 +0300

    initial commit

---
+++ KlivvrSocialAppIOS/KlivvrSocialAppIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KlivvrSocialAppIOS
+//
+//  Created by Eman on 09/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
